2276302be524285ae6af0f0b59bd2a247352f479
Nash Sibanda <contact@nashsibanda.com>
2025-10-26 02:08:27 +0900

    Add batch process (#5)

--- run.py
+++ run.py
@@ -4,10 +4,13 @@
 # ///
 
 import argparse
+from pathlib import Path
 import sys
 
+from subs_merge.batch import batch_extract
 from subs_merge.gui import (
     NoFileSelectedError,
+    run_batch_process_gui,
     run_extract_gui,
     run_merge_gui,
     run_full_process_gui,
@@ -17,7 +20,17 @@ from subs_merge.gui import (
 def main() -> int:
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
-        "command", choices=["extract", "merge", "full"], help="The command to run"
+        "command",
+        choices=["extract", "merge", "full", "batch", "batch-cli"],
+        help="The command to run",
+    )
+    cli_group = parser.add_argument_group("cli", "CLI arguments")
+    cli_group.add_argument(
+        "-d",
+        "--directory-path",
+        default=Path.cwd(),
+        type=Path,
+        help="The directory to process (for batch-cli command)",
     )
     args = parser.parse_args()
     try:
@@ -27,6 +40,13 @@ def main() -> int:
             run_merge_gui()
         elif args.command == "full":
             run_full_process_gui()
+        elif args.command == "batch":
+            run_batch_process_gui()
+        elif args.command == "batch-cli":
+            if args.directory_path is None:
+                print("Error: --directory-path is required for batch-cli command")
+                return 1
+            batch_extract(args.directory_path)
     except NoFileSelectedError as e:
         print(f"Error: {e}")
         return 1
--- subs_merge/full_process.py
+++ subs_merge/full_process.py
@@ -1,20 +1,33 @@
 from tempfile import TemporaryDirectory
 from pathlib import Path
 
+from pymkv import MKVTrack
 from pysubs2.formats import get_file_extension
 from subs_merge.extractor import Extractor
 from subs_merge.merger import Merger
 
 
 def run_full_process(
-    mkv_file_path_str: str, additional_subtitle_file_path_str: str
+    mkv_file_path_str: str,
+    additional_subtitle_file_path_str: str,
+    derive_track_from: MKVTrack | None = None,
 ) -> Path:
     mkv_file_path = Path(mkv_file_path_str)
     additional_subtitle_file_path = Path(additional_subtitle_file_path_str)
     with TemporaryDirectory() as temp_dir:
         temp_dir_path = Path(temp_dir)
         extractor = Extractor(mkv_file_path_str)
-        original_subtitle_file_path = extractor.extract(temp_dir_path)
+        track_id = None
+        if derive_track_from is not None:
+            if derive_track_from.language is None:
+                raise ValueError("Unable to determine language of subtitles..")
+            track_id = extractor.get_most_likely_track_id(
+                language=derive_track_from.language,
+                track_name=derive_track_from.track_name,
+            )
+        original_subtitle_file_path = extractor.extract(
+            temp_dir_path, track_id=track_id
+        )
         merger = Merger(
             Path(original_subtitle_file_path), additional_subtitle_file_path
         )
